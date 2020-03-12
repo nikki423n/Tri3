986eca7ce1a6db7fbaec83499c4b6ec1a0600612
nicole nguyen <nikki423n@gmail.com>
2020-03-12 11:19:04 -0700

    Initial sharing of project

---
+++ Math.java
@@ -0,0 +1,12 @@
+public class Math
+{
+    public static void main(String[] args)
+    {
+        double math = 2/5%3;
+        double math1 = 2/(5%3);
+        double math2 = 2/5 + 1;
+        System.out.println(math);
+        System.out.println(math1);
+        System.out.println(math2);
+    }
+}
---
+++ UnitTwo.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class UnitTwo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class UnitTwo
+{
+    public static void main(String[] args)
+    {
+        int a = 4;
+        int b = 5;
+        
+        double distance = Math.sqrt(a*a + b*b);
+        System.out.println(distance);
+        
+        
+        
+        /* String oldSeq = "0000011";
+        String segment = "11";
+        String newSeq = oldSeq.substring(0, oldSeq.indexOf(segment)) + oldSeq.substring(oldSeq.indexOf(segment) + segment.length());
+        System.out.println(newSeq); */
+        
+      
+    }
+}
+
